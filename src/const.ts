a437c6fb7d9528847179e76f3107462ca754d90e
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2026-08-11 14:41:16 -0300

    :recycle: refactor: Mudando o diretório assets de public para src e ajustando o arquivos const.ts para refletir isso!!!

--- src/const.ts
+++ src/const.ts
@@ -11,9 +11,6 @@ export const ORGANIZATIONAL_PAGES = Object.freeze({
         description: "A materialização escrita de pensamentos, aprendizados e testes de um curioso qualquer.",
         link_description: "Página inicial do Blog Baixo Nível, um blog para curiosos de hardware e programação em geral.",
         thumb: {
-            width: 1200,
-            height: 630,
-            resource: "/assets/home/thumb.png",
             description: "Thumbnail da Página Inicial do Blog Baixo Nível.",
         }
     },
