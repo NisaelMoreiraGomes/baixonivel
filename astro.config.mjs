898130225e2203709b741d616eeffd29fa047b9e
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2026-08-12 19:47:43 -0300

    :wrench: chore: Adicição do gerador de sitemap; Adição do suporte a mdx!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@
 import { defineConfig } from 'astro/config';
 import { SITE_CONFIG } from './src/const';
 
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    site: SITE_CONFIG.url
+    site: SITE_CONFIG.url,
+    integrations: [sitemap(), mdx()]
 });
