9a5f79bd1fab55f24e8c6eae6b0a1f792a237f33
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2026-08-17 14:20:55 -0300

    :recycle: refactor: Melhoria no sistema de animações!!!

---
+++ src/scripts/animation.ts
@@ -0,0 +1,26 @@
+export const animation = {
+    start(animName: string, element: Element | null, removeAnimationOnEnd = true) {
+        element?.classList.remove("hidden")
+        element?.classList.add(animName)
+
+        removeAnimationOnEnd && animation.onEnd(element)
+    },
+
+    end(animName: string, element: Element | null, hiddenOnEnd = true) {
+        element?.classList.add(animName)
+
+        hiddenOnEnd && animation.onEnd(element)
+    },
+
+    onEnd(element: Element | null) {
+        element?.addEventListener("animationend", (e: Event) => {
+            const animName = (e as AnimationEvent).animationName;
+
+            element.classList.remove(animName);
+
+            if (animName.startsWith("desappear")) {
+                element.classList.add("hidden");
+            }
+        })
+    }
+}
