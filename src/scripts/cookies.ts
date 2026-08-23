48e735b55f1bb8b526d1f56ce694374a6e719e21
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2026-08-23 09:22:41 -0300

    :bug: fix: Corrigindo desabilitação da pesquisa; Refatorando ouvinte de evento personalizado!!!

--- src/scripts/cookies.ts
+++ src/scripts/cookies.ts
@@ -1,90 +1,100 @@
-import { eventEmmit } from "./event"
+import { eventEmmit, eventListen } from "./event";
 
-const searchCookieEventName = "searchCookiePermitted"
-const analyticsCookieEventName = "analyticsCookiePermitted"
-const adsCookieEventName = "adsCookiePermitted"
+const searchCookiePermittedEventName = "searchCookiePermitted";
+const searchCookieDeniedEventName = "searchCookieDenied";
+const analyticsCookieEventName = "analyticsCookiePermitted";
+const adsCookieEventName = "adsCookiePermitted";
 
-const adsCookieStorage = "accept-ads-cookies"
-const analitycsCookieStorage = "accept-analitycs-cookies"
-const searchCookieStorage = "accept-search-cookies"
-const hiddenCardCookieStorage = "cookies-version"
+const adsCookieStorage = "accept-ads-cookies";
+const analitycsCookieStorage = "accept-analitycs-cookies";
+const searchCookieStorage = "accept-search-cookies";
+const hiddenCardCookieStorage = "cookies-version";
 
-const stringAccept = (value: boolean) => value ? "y" : "n"
+const stringAccept = (value: boolean) => (value ? "y" : "n");
 
 type CookiesStatus = {
-    ads: boolean;
-    analitycs: boolean;
-    search: boolean;
-}
+  ads: boolean;
+  analitycs: boolean;
+  search: boolean;
+};
 
 export default class Cookies {
-    private static emitted: CookiesStatus = {
-        ads: false,
-        analitycs: false,
-        search: false
+  private static emitted = {
+    ads: false,
+    analitycs: false,
+  };
+
+  private expectedVersion: string;
+
+  constructor(expectedVersion: string) {
+    this.expectedVersion = expectedVersion;
+  }
+
+  set_hidden_card() {
+    localStorage.setItem(hiddenCardCookieStorage, this.expectedVersion);
+  }
+
+  get_hidden_card() {
+    return (
+      localStorage.getItem(hiddenCardCookieStorage) === this.expectedVersion
+    );
+  }
+
+  get_status(): CookiesStatus {
+    return {
+      ads: localStorage.getItem(adsCookieStorage) === "y",
+      analitycs: localStorage.getItem(analitycsCookieStorage) === "y",
+      search: localStorage.getItem(searchCookieStorage) === "y",
+    };
+  }
+
+  set_and_emit_status(status: CookiesStatus) {
+    this.set_status(status);
+    this.emit_status(status);
+  }
+
+  set_status(status: CookiesStatus) {
+    localStorage.setItem(adsCookieStorage, stringAccept(status.ads));
+    localStorage.setItem(
+      analitycsCookieStorage,
+      stringAccept(status.analitycs),
+    );
+    localStorage.setItem(searchCookieStorage, stringAccept(status.search));
+
+    this.set_hidden_card();
+  }
+
+  emit_status(status: CookiesStatus) {
+    if (status.ads && !Cookies.emitted.ads) {
+      Cookies.emitted.ads = true;
+      eventEmmit(adsCookieEventName);
     }
 
-    private expectedVersion: string
-
-    constructor(expectedVersion: string) {
-        this.expectedVersion = expectedVersion
+    if (status.analitycs && !Cookies.emitted.analitycs) {
+      Cookies.emitted.analitycs = true;
+      eventEmmit(analyticsCookieEventName);
     }
 
-    set_hidden_card() {
-        localStorage.setItem(hiddenCardCookieStorage, this.expectedVersion)
+    if (status.search) {
+      eventEmmit(searchCookiePermittedEventName);
+    } else {
+      eventEmmit(searchCookieDeniedEventName);
     }
+  }
 
-    get_hidden_card() {
-        return localStorage.getItem(hiddenCardCookieStorage) === this.expectedVersion
-    }
+  static onSearchPermitted(action: () => void) {
+    eventListen(searchCookiePermittedEventName, action);
+  }
 
-    get_status(): CookiesStatus {
-        return {
-            ads: localStorage.getItem(adsCookieStorage) === "y",
-            analitycs: localStorage.getItem(analitycsCookieStorage) === "y",
-            search: localStorage.getItem(searchCookieStorage) === "y"
-        }
-    }
+  static onSearchDenied(action: () => void) {
+    eventListen(searchCookieDeniedEventName, action);
+  }
 
-    set_and_emit_status(status: CookiesStatus) {
-        this.set_status(status)
-        this.emit_status(status)
-    }
+  static onAnalitycsPermitted(action: () => void) {
+    eventListen(analyticsCookieEventName, action);
+  }
 
-    set_status(status: CookiesStatus) {
-        localStorage.setItem(adsCookieStorage, stringAccept(status.ads))
-        localStorage.setItem(analitycsCookieStorage, stringAccept(status.analitycs))
-        localStorage.setItem(searchCookieStorage, stringAccept(status.search))
-
-        this.set_hidden_card()
-    }
-
-    emit_status(status: CookiesStatus) {
-        if (status.ads && !Cookies.emitted.ads) {
-            Cookies.emitted.ads = true
-            eventEmmit(adsCookieEventName)
-        }
-
-        if (status.analitycs && !Cookies.emitted.analitycs) {
-            Cookies.emitted.analitycs = true
-            eventEmmit(analyticsCookieEventName)
-        }
-
-        if (status.search && !Cookies.emitted.search) {
-            Cookies.emitted.search = true
-            eventEmmit(searchCookieEventName)
-        }
-    }
-
-    static onSearchPermitted(action: () => void) {
-        document.addEventListener(searchCookieEventName, action)
-    }
-
-    static onAnalitycsPermitted(action: () => void) {
-        document.addEventListener(analyticsCookieEventName, action)
-    }
-
-    static onAdsPermitted(action: () => void) {
-        document.addEventListener(adsCookieEventName, action)
-    }
+  static onAdsPermitted(action: () => void) {
+    eventListen(adsCookieEventName, action);
+  }
 }
--- src/scripts/event.ts
+++ src/scripts/event.ts
@@ -1,1 +1,5 @@
-export const eventEmmit = (name: string, detail = {}) => document.dispatchEvent(new CustomEvent(name, { detail }))
+export const eventEmmit = (name: string, detail = {}) =>
+  document.dispatchEvent(new CustomEvent(name, { detail }));
+export const eventListen = (name: string, action: (event?: Event) => void) => {
+  document.addEventListener(name, action);
+};
