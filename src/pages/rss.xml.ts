f539e520a076142d8e99dfb7ede1040a9840e6b4
NisaelMoreiraGomes <gomes.nisaelmoreira@gmail.com>
2026-08-23 09:42:59 -0300

    :sparkles: feat: Adição do Feed RSS; Definição da Coleção Blog!!!

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_CONFIG, HOME_PAGE } from "../const";
+import type { APIContext } from "astro";
+
+export async function GET({ site }: APIContext) {
+  if (!site) {
+    throw new Error("The site context was not found.");
+  }
+
+  const posts = await getCollection("blog");
+  return rss({
+    title: SITE_CONFIG.name,
+    description: HOME_PAGE.description,
+    site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id}/`,
+    })),
+  });
+}
